ff99e67a741ea7f7182d2383f52fd9d56068dd87
thien pham <47543920+ThienTheCreator@users.noreply.github.com>
2022-12-11 01:08:43 -0800

    work on displaying game

--- script.js
+++ script.js
@@ -17,8 +17,9 @@ const img = document.get
 
 let canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 160;
-canvas.height = 160;
+ctx.imageSmoothingEnabled = false;
+canvas.width = 177;
+canvas.height = 178;
 
 // canvas.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
 // canvas.fillRect( 1, 1, 1, 1 );
@@ -27,12 +28,43 @@ const myImgElement = document.getElementById("myImag")
 
 // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
 // empty box
-// ctx.drawImage( myImgElement, 14, 195, 16, 16, 0, 0, 16, 16);
+// Example: ctx.drawImage( myImgElement, 14, 195, 16, 16, 0, 0, 16, 16);
+ctx.imageSmoothingEnabled = false;
+ctx.drawImage( myImgElement, 478, 422, 9, 9, 0, 0, 9, 9);
+for(let i = 0; i < 10; i ++){
+  ctx.imageSmoothingEnabled = false;
+  ctx.drawImage( myImgElement, 487, 422, 16, 9, 9+i*16, 0, 16, 9);
+}
+
+ctx.imageSmoothingEnabled = false;
+ctx.drawImage( myImgElement, 743, 422, 8, 9, 169, 0, 8, 9);
+for(let i = 0; i < 10; i++){
+  ctx.imageSmoothingEnabled = false;
+  ctx.drawImage( myImgElement, 743, 431, 8, 16, 169, 9+i*16, 8, 16);
+}
+
+ctx.imageSmoothingEnabled = false;
+ctx.drawImage( myImgElement, 743, 687, 8, 8, 169, 169, 8, 8);
+for(let i = 0; i < 10; i++){
+  ctx.imageSmoothingEnabled = false;
+  ctx.drawImage( myImgElement, 727, 687, 16, 8, 153-i*16, 169, 16, 8);
+}
+
+ctx.imageSmoothingEnabled = false;
+ctx.drawImage( myImgElement, 478, 687, 9, 8, 0, 169, 9, 8);
+for(let i = 0; i < 10; i++){
+  ctx.imageSmoothingEnabled = false;
+  ctx.drawImage( myImgElement, 478, 671, 9, 16, 0, 153-i*16, 9, 16);
+}
+
 for(let i = 0; i < 160; i += 16){
     for(let j = 0; j < 160; j += 16){
-        ctx.drawImage( myImgElement, 14, 195, 16, 16, i, j, 16, 16);
+        ctx.drawImage( myImgElement, 14, 195, 16, 16, i+9, j+9, 16, 16);
     }
 }
+
+
+
 
 function getMousePosition(canvas, event) {
     let rect = canvas.getBoundingClientRect();
